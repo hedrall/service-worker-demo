3d6b39604a98c31684a2fd206b4f2c70e6b07038
hedrall <masaharu73@gmail.com>
2021-05-20 07:53:02 +0900

    キャッシュを追記

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,20 @@
+const CACHE_NAME = 'my-test-cache';
+const URLS_TO_CACHE = [
+  './assets/for-cache.js',
+  './assets/for-cache.css',
+]
+
 addEventListener('install', e => {
+  // 2. サービスワーカーをインストール
+  // 初回のみ呼ばれる
   console.log('install event');
   console.log(e);
+
+  // ファイルとキャッシュしてみる
+  e.waitUntil(async () => {
+    const cache = await caches.open(CACHE_NAME);
+    console.log('キャッシュを開く');
+    await cache.addAll(URLS_TO_CACHE);
+    console.log('キャッシュ完了');
+  })
 })

67d79e907cf83b36a83efb757a83ed85e2c3bc61
hedrall <masaharu73@gmail.com>
2021-05-20 11:27:56 +0900

    fix

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,15 +1,16 @@
+const VERSION = 10;
 const CACHE_NAME = 'my-test-cache';
 const URLS_TO_CACHE = [
   '/service-worker-demo/assets/for-cache.js',
   // './assets/for-cache.css',
   // './not-exist.css',
-]
+];
 
 // 初回のインストール時のイベント
 addEventListener('install', function (event) {
   // 2. サービスワーカーをインストール
   // 初回のみ呼ばれる
-  console.log('installイベント');
+  console.log('installイベント', `VERSION ${VERSION}`);
 
   // ファイルをキャッシュしてみる
   event.waitUntil(
@@ -33,4 +34,9 @@ addEventListener('fetch', (event) => {
       return fetch(event.request);
     })
   )
+});
+
+// 更新後のservice-worker起動時の設定
+addEventListener('activate', event => {
+  console.log(`new service-worker VERSION ${VERSION} activated`)
 });
